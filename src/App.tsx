6a99cfbf6bd1751c8ab2d319f6e20c79c924b009
Lenodeoliveira <lenodeoliveira@gmail.com>
2021-11-14 17:46:44 -0300

    Start challenge

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import './App.scss'
 
 const App: React.FC = () => {
-	const logoMoovin =
-		'https://cdn.moovin.com.br/project/manager-panel/img/logo-moovin.svg'
 
 	return (
 		<div className='App'>
